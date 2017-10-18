319dd95928d167612e71a9ebe2a0db703907c0a4
Nukepayload2 <1939357182@qq.com>
2017-10-19 01:20:12 +0800

    添加测试用的项目

---
+++ Nukepayload2.AppService.DesktopTestBroker/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+
+    End Sub
+End Class
---
+++ Nukepayload2.AppService.UwpTestApp/Properties/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Nukepayload2.AppService.UwpTestApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Nukepayload2.AppService.UwpTestApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+<assembly: AssemblyMetadata("TargetPlatform","UAP")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: ComVisible(False)>
